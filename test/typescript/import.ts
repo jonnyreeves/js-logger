b2d293dc79f46e8e8aef6ec9a542a322e412227c
John Reeves <github@jonnyreeves.co.uk>
2018-02-19 06:59:53 +0000

    Add assertions for node and typescript integration tests (#78)
    - Move entrypoint to `index.ts`
    - Minimise the number of exports
    - Ensure that an Array is always passed to LogHandler funcs
    - Add golden record validation for integration tests

--- test/typescript/import.ts
+++
@@ -1,8 +0,0 @@
-import jslogger from '../..';
-
-jslogger.useDefaults();
-jslogger.info('hello world!');
-jslogger.error('bang!');
-
-const namedLogger = jslogger.get('myLogger');
-namedLogger.info('hello from namedLogger :)');
--- test/typescript/src/import.ts
+++ test/typescript/src/import.ts
@@ -1,28 +1,28 @@
-const jslogger = require('../../').default;
+import jslogger, { IContext, ILogger, LogHandler } from '../../..';
 
 jslogger.useDefaults();
 jslogger.info('hello world!');
 jslogger.error('bang!');
 
-const namedLogger = jslogger.get('myLogger');
+const namedLogger: ILogger = jslogger.get('myLogger');
 namedLogger.info('hello from namedLogger :)');
 
 // Install a custom log handler which pushes to an Array instead of
 // writing to the console.
-const receivedMessages = [];
-const customHandler =  (messages, context) => {
+const receivedMessages: any[] = [];
+const customHandler: LogHandler =  (messages: any[], context: IContext) => {
     receivedMessages.push(messages);
 }
 jslogger.setHandler(customHandler);
 jslogger.info('custom handler test');
  receivedMessages.forEach(element => {
-    console.log('received message', ...element);
+     console.log('received message', ...element);
  });
 
  // Install a custom log handler, based on the default one which prefixes
  // all log messages before writing to the console.
- const prefixedDefaultHandler = jslogger.createDefaultHandler({
-     formatter: (messages, context) => {
+ const prefixedDefaultHandler: LogHandler = jslogger.createDefaultHandler({
+     formatter: (messages: any[], context: IContext) => {
          messages.unshift('my_prefix');
      }
  })
